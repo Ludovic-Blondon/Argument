18066f15ffe4339ab4bb36e65730c27cfcbfafca
lblondon <ludovicblondon@gmail.com>
2025-09-20 16:45:48 +0200

    Initial Commit

---
+++ ArgumentTests/ArgumentTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArgumentTests.swift
+//  ArgumentTests
+//
+//  Created by Ludovic Blondon on 20/09/2025.
+//
+
+import Testing
+@testable import Argument
+
+struct ArgumentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
